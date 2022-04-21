8c94e31fc6e60a6aae3d064933fc08e5072f7c51
kowshal <kowshal2103@gmail.com>
2022-04-21 19:57:32 +0530

    three components

---
+++ src/Components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+        <h2 className='name'>S Kowshal</h2>
+        <h4 className='clg'>Vellore Institute of Technology</h4>
+        <h5 className='degree'>B.Tech-Computer Science</h5>
+        <h3 className='tag'>"Proud Indian"</h3>
+        <img className='my-img' src='./me.jpeg' alt='Its me' />
+    </div>
+  )
+}
